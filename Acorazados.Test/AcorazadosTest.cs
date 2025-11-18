3c2175ab86b7158f30496442a2ba058de8b535b6
Diego Alejandro Gonzalez Cantor <Diego.gonzalez@sinco.com.co>
2025-11-18 10:28:30 -0500

    🟥 Si_CreoUnTablero_Debe_EstarSinBarcos

--- Acorazados.Test/AcorazadosTest.cs
+++ Acorazados.Test/AcorazadosTest.cs
@@ -1,9 +1,29 @@
-﻿namespace Acorazados.Test;
+﻿using AwesomeAssertions;
+
+namespace Acorazados.Test;
 
 public class AcorazadosTest
 {
     [Fact]
-    public void Test1()
+    public void Si_CreoUnTablero_Debe_EstarSinBarcos()
     {
+        var acorazados = new Acorazados();
+
+        acorazados.CrearTablero();
+
+        acorazados.CantidadBarcos.Should().Be(0);
+    }
+
+    public class Acorazados
+    {
+        
+        public object CantidadBarcos { get; set; }
+
+        public void CrearTablero()
+        {
+            throw new NotImplementedException();
+        }
+
+        
     }
 }
---
+++ Acorazados.Test/TestList.md
@@ -0,0 +1,12 @@
+﻿- [] Si inicializo un tablero, debe estar sin barcos
+Puedo colocar un barco en el tablero
+
+Un cañonero debe ocupar una posición
+Un destructor debe ocupar tres posiciones
+Un portaviones debe ocupar cuatro posiciones
+
+Si disparo al tablero y le doy al agua, debe mostrar en mensaje 'Tiro fallido'
+Si disparo al tablero y le doy a un barco, debe mostrar en mensaje 'Tiro exitoso'
+
+
+Un barco no puede estar posicionado sobre otro barco
