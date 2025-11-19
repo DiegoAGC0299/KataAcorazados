9ee79ee41347d4277fb61c340fc7e5b57cb7a65e
Diego Alejandro Gonzalez Cantor <Diego.gonzalez@sinco.com.co>
2025-11-19 16:59:59 -0500

    🟦 Si_AgregoMasDeDosJugadores_Debe_LanzarExcepcion

--- Acorazados.Test/Acorazados.cs
+++ Acorazados.Test/Acorazados.cs
@@ -2,20 +2,18 @@
 
 public class Acorazados
 {
-    public List<Jugador> Jugadores { get; } = [];
+    private List<Jugador> _jugadores { get; } = [];
 
     public void AgregarJugador(string nombre)
     {
-        if(Jugadores.Count == 2)
+        if(_jugadores.Count == 2)
             throw new InvalidOperationException("No se pueden agregar más de dos jugadores");
         
-        Jugadores.Add(new Jugador(nombre));
+        _jugadores.Add(new Jugador(nombre));
     }
 
     public Jugador ObtenerJugador(int indice)
-    {
-        return new Jugador("David");
-    }
+        =>  _jugadores[indice]; 
 }
 
 public class Jugador(string nombre)
