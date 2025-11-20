71262a77c08d453a2b30f2e26afe6e00dbdd05f5
Diego Alejandro Gonzalez Cantor <Diego.gonzalez@sinco.com.co>
2025-11-20 18:16:18 -0500

    🟥 Si_Jugador1DisparaDosVecesYUnoDeLosTirosEsFallidoYElJuegoSeHaFinalizadoYSeImprimeReporteJugadorDos_Debe_TotalDeDisparosSer2YTotalDeTirosFallidosSer1

--- Acorazados.Test/AcorazadosTest.cs
+++ Acorazados.Test/AcorazadosTest.cs
@@ -429,4 +429,29 @@ public class AcorazadosTest
         reporteGenerado.Should().Contain("Disparos totales: 2");
 
     }
+    
+    [Fact]
+    public void Si_Jugador1DisparaDosVecesYUnoDeLosTirosEsFallidoYElJuegoSeHaFinalizadoYSeImprimeReporteJugadorDos_Debe_TotalDeDisparosSer2YTotalDeTirosFallidosSer1()
+    {
+        var acorazados = _acorazadosBuilder
+            .ConstruirJugadorUno("David", tablero =>
+            {
+                tablero.AgregarBarco(Barcos.Canonero, 1,1);
+            } )
+            .ConstruirJugadorDos("Diego", tablero =>
+            {
+                tablero.AgregarBarco(Barcos.Canonero, 1,1);
+            }).Construir();
+        acorazados.Iniciar();
+        acorazados.Disparar(1, 3);
+        acorazados.Disparar(1, 2);
+        acorazados.Disparar(1, 1);
+        var jugador2 = acorazados.ObtenerJugador(1);
+        
+        var reporteGenerado = acorazados.ImprimirReporte(jugador2);
+        
+        reporteGenerado.Should().Contain("Disparos totales: 2");
+        reporteGenerado.Should().Contain("Fallidos: 1");
+
+    }
 }
